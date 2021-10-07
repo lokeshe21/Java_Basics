4272b11a92b901f53560ac33926278ab834d9210
lokeshe21 <66429225+lokeshe21@users.noreply.github.com>
2021-10-08 00:05:19 +0530

    Add files via upload

---
+++ WhileDemo1.java
@@ -0,0 +1,19 @@
+package Java.WhileDemo;
+
+public class WhileDemo1 {
+
+	public static void main(String[] args) {
+		
+		int i=0,x=2,y=2;
+		
+		while(i<5){
+			System.out.print(x+" ");
+			i=i+1;
+			x=x+(y+2);
+			y=y+2;
+			}
+		
+
+	}
+
+}
